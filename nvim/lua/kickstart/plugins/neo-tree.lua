ecf6983b6b11e797a723c307b71535b0f959b7be
han-ubuntu <gwu.suomi@outlook.com>
2024-10-09 00:53:56 +0300

    re-init

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,192 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', '<cmd>Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { '<leader>e', '<cmd>Neotree toggle<CR>', desc = 'Toggle NeoTree' },
+  },
+  opts = {
+    close_if_last_window = false,
+    popup_border_style = 'rounded',
+    enable_git_status = true,
+    enable_diagnostics = true,
+    sort_case_insensitive = false,
+    sort_function = nil,
+    default_component_configs = {
+      container = {
+        enable_character_fade = true,
+      },
+      indent = {
+        indent_size = 2,
+        padding = 1,
+        with_markers = true,
+        indent_marker = '│',
+        last_indent_marker = '└',
+        highlight = 'NeoTreeIndentMarker',
+        with_expanders = nil,
+        expander_collapsed = '',
+        expander_expanded = '',
+        expander_highlight = 'NeoTreeExpander',
+      },
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = 'ﰊ',
+        default = '*',
+        highlight = 'NeoTreeFileIcon',
+      },
+      modified = {
+        symbol = '[+]',
+        highlight = 'NeoTreeModified',
+      },
+      name = {
+        trailing_slash = false,
+        use_git_status_colors = true,
+        highlight = 'NeoTreeFileName',
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = '✚',
+          deleted = '✖',
+          modified = '',
+          renamed = '󰁕',
+          -- Status type
+          untracked = '',
+          ignored = '',
+          unstaged = '󰄱',
+          staged = '',
+          conflict = '',
+        },
+      },
+    },
+    window = {
+      position = 'left',
+      width = 30,
+      mapping_options = {
+        noremap = true,
+        nowait = true,
+      },
+      mappings = {
+        ['<space>'] = {
+          'toggle_node',
+          nowait = false,
+        },
+        ['<2-LeftMouse>'] = 'open',
+        ['<cr>'] = 'open',
+        ['<esc>'] = 'revert_preview',
+        ['P'] = { 'toggle_preview', config = { use_float = true } },
+        ['l'] = 'focus_preview',
+        ['S'] = 'open_split',
+        ['s'] = 'open_vsplit',
+        ['t'] = 'open_tabnew',
+        ['w'] = 'open_with_window_picker',
+        ['C'] = 'close_node',
+        ['z'] = 'close_all_nodes',
+        ['a'] = {
+          'add',
+          config = {
+            show_path = 'none',
+          },
+        },
+        ['A'] = 'add_directory',
+        ['d'] = 'delete',
+        ['r'] = 'rename',
+        ['y'] = 'copy_to_clipboard',
+        ['x'] = 'cut_to_clipboard',
+        ['p'] = 'paste_from_clipboard',
+        ['c'] = 'copy',
+        ['m'] = 'move',
+        ['q'] = 'close_window',
+        ['R'] = 'refresh',
+        ['?'] = 'show_help',
+        ['<'] = 'prev_source',
+        ['>'] = 'next_source',
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = false, -- Changed from true to false
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_hidden = true,
+        hide_by_name = {
+          'node_modules',
+        },
+        hide_by_pattern = {
+          '*.meta',
+          '*/src/*/tsconfig.json',
+        },
+        always_show = {
+          '.gitignore',
+        },
+        never_show = {
+          '.DS_Store',
+          'thumbs.db',
+        },
+        never_show_by_pattern = {
+          '.null-ls_*',
+        },
+      },
+      follow_current_file = true, -- Changed from false to true
+      group_empty_dirs = false,
+      hijack_netrw_behavior = 'open_default',
+      use_libuv_file_watcher = true, -- Changed from false to true
+      window = {
+        mappings = {
+          ['<bs>'] = 'navigate_up',
+          ['.'] = 'set_root',
+          ['H'] = 'toggle_hidden',
+          ['/'] = 'fuzzy_finder',
+          ['D'] = 'fuzzy_finder_directory',
+          ['#'] = 'fuzzy_sorter',
+          ['f'] = 'filter_on_submit',
+          ['<c-x>'] = 'clear_filter',
+          ['[g'] = 'prev_git_modified',
+          [']g'] = 'next_git_modified',
+        },
+        fuzzy_finder_mappings = {
+          ['<down>'] = 'move_cursor_down',
+          ['<C-n>'] = 'move_cursor_down',
+          ['<up>'] = 'move_cursor_up',
+          ['<C-p>'] = 'move_cursor_up',
+        },
+      },
+    },
+    buffers = {
+      follow_current_file = true,
+      group_empty_dirs = true,
+      show_unloaded = true,
+      window = {
+        mappings = {
+          ['bd'] = 'buffer_delete',
+          ['<bs>'] = 'navigate_up',
+          ['.'] = 'set_root',
+        },
+      },
+    },
+    git_status = {
+      window = {
+        position = 'float',
+        mappings = {
+          ['A'] = 'git_add_all',
+          ['gu'] = 'git_unstage_file',
+          ['ga'] = 'git_add_file',
+          ['gr'] = 'git_revert_file',
+          ['gc'] = 'git_commit',
+          ['gp'] = 'git_push',
+          ['gg'] = 'git_commit_and_push',
+        },
+      },
+    },
+  },
+}
